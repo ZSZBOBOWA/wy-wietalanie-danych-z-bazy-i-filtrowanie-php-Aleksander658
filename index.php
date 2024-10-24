d5b9bbaa7f2508c402833b9b8851f19cb65b6b1d
Aleksander Obrzut <EMAIL>
2024-10-24 08:48:35 +0200

    modyfikacja kodu php

--- index.php
+++ index.php
@@ -6,38 +6,58 @@
     <title>Formularz</title>
 </head>
 <body>
-    <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "szkola2";
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    if (!$conn) {
-        die("Błąd połączenia: " . mysqli_connect_error());
-    }
-    ?>
-
     <form method="POST" action="index.php">
         Wpisz nazwisko: <input type="text" name="nazwisko">
         <input type="submit" value="Filtruj">
     </form>
 
     <?php
-    $sql = "SELECT * FROM uczniowie";
-    $result = mysqli_query($conn, $sql);
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "szkola2";
 
-    if (mysqli_num_rows($result) > 0) {
-        echo "<table border='1'><tr><th>Imię</th><th>Nazwisko</th><th>Wiek</th></tr>";
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr><td>" . $row["Imie"] . "</td><td>" . $row["Nazwisko"] . "</td><td>" . $row["Wiek"] . "</td></tr>";
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        if (!$conn) {
+            die("Błąd połączenia: " . mysqli_connect_error());
         }
-        echo "</table>";
-    } else {
-        echo "Brak wyników";
-    }
-    mysqli_close($conn);
+
+        if(isset($_POST["nazwisko"])&&$_POST["nazwisko"]!="") {
+            $nazwisko = $_POST["nazwisko"];
+
+            $nazwisko = mysqli_real_escape_string($conn, $nazwisko);
+
+            $sql = "SELECT * FROM uczniowie WHERE Nazwisko='$nazwisko'";
+        } else {
+            $sql = "SELECT * FROM uczniowie";
+        }
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                echo "<table border='1'><tr><th>Imię</th><th>Nazwisko</th><th>Wiek</th></tr>";
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr><td>" . $row["Imie"] . "</td><td>" . $row["Nazwisko"] . "</td><td>" . $row["Wiek"] . "</td></tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "Brak wyników";
+            }
+        }
+
+        $sql = "SELECT * FROM uczniowie";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            echo "<table border='1'><tr><th>Imię</th><th>Nazwisko</th><th>Wiek</th></tr>";
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr><td>" . $row["Imie"] . "</td><td>" . $row["Nazwisko"] . "</td><td>" . $row["Wiek"] . "</td></tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "Brak wyników";
+        }
+        mysqli_close($conn);
     ?>
 </body>
 </html>
